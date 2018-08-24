360d9a58be290b9b49a803677255c1c1142ce6a2
joe <joe@joes-MacBook-Pro.local>
2018-08-24 15:23:51 +0100

    added side effect to mocking

--- use_simple_function.py
+++ use_simple_function.py
@@ -3,19 +3,19 @@ from unittest import mock
 
 import simple
 
-def use_simple_function():
-    result = simple.simple_function()
-    print(result)
-
-use_simple_function()
-
-@mock.patch('simple.simple_function')
-def mock_simple_function(mock_simple_func):
-    mock_simple_func.return_value = "You have mocked simple_function"
-    result = simple.simple_function()
-    print(result)
-
-mock_simple_function()
+# def use_simple_function():
+#     result = simple.simple_function()
+#     print(result)
+#
+# use_simple_function()
+#
+# @mock.patch('simple.simple_function')
+# def mock_simple_function(mock_simple_func):
+#     mock_simple_func.return_value = "You have mocked simple_function"
+#     result = simple.simple_function()
+#     print(result)
+#
+# mock_simple_function()
 
 # output:
 # You have called simple_function
@@ -41,3 +41,12 @@ mock_simple_function()
 
 # So when MagicMock is called as a function (in the form of simple_function here), the __call__ function
 # is called, and magicmock implements this as a new MagicMock object
+
+def side_effect_function():
+    return "Woooh!"
+
+@mock.patch('simple.simple_function')
+def mock_simple_function_with_side_effect(mock_simple_func):
+    mock_simple_func.side_effect = side_effect_function
+    result = simple.simple_function()
+    print(result)
